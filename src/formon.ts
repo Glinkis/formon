9c14b8f83e642cc350d12a6785b679e7122163b1
Victor Glindås <glinkis@gmail.com>
2025-11-18 22:08:24 +0100

    feat: add basic implementation

--- src/formon.ts
+++ src/formon.ts
@@ -1,1 +1,209 @@
-console.log("Hello via Bun!");
+const NON_SHARED_VALUE = Symbol("NON_SHARED_VALUE");
+
+type NonOptional<TValue> = TValue extends undefined ? never : TValue;
+
+type TDefaultValues<TObject> = TObject | { [TName in keyof TObject]?: TDefaultValues<TObject[TName]> | null };
+
+type FormHelperOptions<TObject> = {
+  defaultValues?: TDefaultValues<TObject> | TDefaultValues<TObject>[];
+};
+
+type InputProps = {
+  name: string;
+  defaultValue?: string;
+  defaultChecked?: boolean;
+  readOnly?: boolean;
+  className?: string;
+};
+
+type GetInputProps = () => InputProps;
+
+/**
+ * Used to match the default value to the correct indexed field.
+ */
+type GetIndex<TValue> = (defualtValue: TValue) => number;
+
+/** */
+type RegisteredNestedInput<TValue> = {
+  [TName in keyof TValue]-?: RegisterInput<TValue[TName]>;
+};
+
+type RegisteredIndexedInput<TValue> = {
+  getIndex: (index: number, getIndex?: GetIndex<TValue>) => RegisterInput<TValue>;
+};
+
+/**
+ * Infers nested input registration accessors from the object shape.
+ */
+type RegisterInput<TValue> = TValue extends (infer TArrayItem)[]
+  ? RegisteredIndexedInput<TArrayItem>
+  : TValue extends object
+    ? RegisteredNestedInput<TValue>
+    : GetInputProps;
+
+type RegisteredNestedInputName<TValue, Path extends string> = {
+  [TName in keyof TValue]: TName extends string
+    ? RegisteredInputName<TValue[TName], Path extends "" ? TName : `${Path}.${TName}`>
+    : never;
+}[keyof TValue];
+
+type RegisteredIndexedInputName<TValue, Path extends string> = RegisteredInputName<TValue, `${Path}[${string}]`>;
+
+/**
+ * Infers nested input name strings from the object shape.
+ * Should have the same general structure as `RegisterInput`.
+ */
+type RegisteredInputName<TValue, Path extends string> = TValue extends (infer TArrayItem)[]
+  ? RegisteredIndexedInputName<TArrayItem, Path>
+  : NonOptional<TValue> extends object
+    ? RegisteredNestedInputName<TValue, Path>
+    : Path;
+
+type NestedPath = Array<string | number | { index: number; getIndex: GetIndex<unknown> }>;
+
+type FormHelper<TObject> = {
+  register: RegisterInput<TObject>;
+};
+
+/**
+ * Creates a form helper object that can be used to register form inputs and submit the form.
+ */
+export function createFormHelper<TObject>(options: FormHelperOptions<TObject> = {}): FormHelper<NoInfer<TObject>> {
+  return {
+    register: createNestedGetProps<TObject>([], options.defaultValues),
+  };
+}
+
+export function createNestedGetProps<TNested>(path: NestedPath, defaultValues: unknown): RegisterInput<TNested> {
+  const proxied = (() => {}) as RegisterInput<TNested>;
+
+  return new Proxy(proxied, {
+    apply: (_, __, args) => {
+      // @ts-ignore Allow dynamic arguments.
+      return createGetProps(path, defaultValues)(...args);
+    },
+    get: (_, prop) => {
+      if (typeof prop === "symbol") {
+        throw new Error("Symbol properties are not supported");
+      }
+
+      if (prop === "getIndex") {
+        return (index: number, getIndex?: GetIndex<unknown>) => {
+          if (typeof getIndex === "function") {
+            return createNestedGetProps([...path, { index, getIndex }], defaultValues);
+          }
+
+          return createNestedGetProps([...path, index], defaultValues);
+        };
+      }
+
+      return createNestedGetProps([...path, prop], defaultValues);
+    },
+  });
+}
+
+function createGetProps(path: NestedPath, defaultValues: unknown): GetInputProps {
+  return function getProps(): InputProps {
+    const name = getNameFromPath(path);
+    const defaultValue = getMaybeSharedDefaultValue(path, defaultValues);
+
+    if (defaultValue === NON_SHARED_VALUE) {
+      return {
+        name: name,
+        readOnly: true,
+      };
+    }
+
+    if (typeof defaultValue === "boolean") {
+      return {
+        name: name,
+        defaultChecked: defaultValue,
+      };
+    }
+
+    if (typeof defaultValue === "string") {
+      return {
+        name: name,
+        defaultValue: defaultValue,
+      };
+    }
+
+    if (typeof defaultValue === "number") {
+      return {
+        name: name,
+        defaultValue: defaultValue.toString(),
+      };
+    }
+
+    return {
+      name: name,
+    };
+  };
+}
+
+function getNameFromPath(path: NestedPath) {
+  let name = "";
+
+  for (const value of path) {
+    if (name === "") {
+      name += value;
+      continue;
+    }
+
+    if (typeof value === "string") {
+      name += `.${value}`;
+      continue;
+    }
+
+    if (typeof value === "number") {
+      name += `[${value}]`;
+      continue;
+    }
+
+    if (typeof value === "object") {
+      name += `[${value.index}]`;
+    }
+  }
+
+  return name;
+}
+
+function getMaybeSharedDefaultValue(path: NestedPath, defaultValues: unknown) {
+  // This part is a bit tricky, as we need to figure out non-shared
+  // default values for nested objects in multi-edit forms.
+  if (Array.isArray(defaultValues)) {
+    const [firstDefaultValues, ...otherDefaultValues] = defaultValues;
+    const firstDefaultValue = getDefaultValue(path, firstDefaultValues);
+
+    for (const otherDefaultValue of otherDefaultValues) {
+      const currentDefaultValue = getDefaultValue(path, otherDefaultValue);
+
+      if (currentDefaultValue !== firstDefaultValue) {
+        return NON_SHARED_VALUE;
+      }
+    }
+
+    return firstDefaultValue;
+  }
+
+  return getDefaultValue(path, defaultValues);
+}
+
+function getDefaultValue(path: NestedPath, defaultValues: unknown) {
+  for (const value of path) {
+    if (defaultValues == null) {
+      break;
+    }
+
+    if (typeof value === "object") {
+      defaultValues = (defaultValues as unknown[]).find((defaultValue) => {
+        return value.index === value.getIndex(defaultValue);
+      });
+      continue;
+    }
+
+    defaultValues = (defaultValues as Record<string, unknown>)[value];
+  }
+
+  return defaultValues;
+}
--- test/formon.test.ts
+++ test/formon.test.ts
@@ -1,5 +1,209 @@
-import { it } from "bun:test";
+import { expect, it } from "bun:test";
+import { createFormHelper } from "../src/formon";
 
-it.todo("write tests for formon", () => {
-  // Placeholder for future tests
+it("can handle a flat object shape", () => {
+  interface Shape {
+    id: number;
+    name: string;
+    isUser: boolean;
+    isAdmin: boolean;
+    isSuperAdmin: boolean;
+  }
+
+  const form = createFormHelper<Shape>({
+    defaultValues: {
+      name: "John",
+      isUser: true,
+      isAdmin: false,
+    },
+  });
+
+  expect(form.register.id()).toEqual({
+    name: "id",
+  });
+
+  expect(form.register.name()).toEqual({
+    name: "name",
+    defaultValue: "John",
+  });
+
+  expect(form.register.isUser()).toEqual({
+    name: "isUser",
+    defaultChecked: true,
+  });
+
+  expect(form.register.isAdmin()).toEqual({
+    name: "isAdmin",
+    defaultChecked: false,
+  });
+
+  expect(form.register.isSuperAdmin()).toEqual({
+    name: "isSuperAdmin",
+  });
+});
+
+it("can handle a flat object shape with multiple default values", () => {
+  interface Shape {
+    id: number;
+    name: string;
+    age: number;
+  }
+
+  const form = createFormHelper<Shape>({
+    defaultValues: [
+      { name: "John", age: 18 },
+      { name: "Jane", age: 18 },
+    ],
+  });
+
+  expect(form.register.id()).toEqual({
+    name: "id",
+  });
+
+  expect(form.register.name()).toEqual({
+    name: "name",
+    readOnly: true,
+  });
+
+  expect(form.register.age()).toEqual({
+    name: "age",
+    defaultValue: "18",
+  });
+});
+
+it("can handle a nested object shape", () => {
+  interface Shape {
+    id: number;
+    user: {
+      id: number;
+      name: string;
+      sites: string[];
+    };
+  }
+
+  const form = createFormHelper<Shape>({
+    defaultValues: {
+      user: {
+        name: "John",
+        sites: ["site1", "site2"],
+      },
+    },
+  });
+
+  expect(form.register.id()).toEqual({
+    name: "id",
+  });
+
+  expect(form.register.user.id()).toEqual({
+    name: "user.id",
+  });
+
+  expect(form.register.user.name()).toEqual({
+    name: "user.name",
+    defaultValue: "John",
+  });
+
+  expect(form.register.user.sites.getIndex(0)()).toEqual({
+    name: "user.sites[0]",
+    defaultValue: "site1",
+  });
+
+  expect(form.register.user.sites.getIndex(1)()).toEqual({
+    name: "user.sites[1]",
+    defaultValue: "site2",
+  });
+});
+
+it("can handle a nested indexed object shape", () => {
+  const form = createFormHelper({
+    defaultValues: {
+      users: [{ name: "John" }, { name: "Jane" }],
+    },
+  });
+
+  expect(form.register.users.getIndex(0).name()).toEqual({
+    name: "users[0].name",
+    defaultValue: "John",
+  });
+
+  expect(form.register.users.getIndex(1).name()).toEqual({
+    name: "users[1].name",
+    defaultValue: "Jane",
+  });
+});
+
+it("can handle a nested indexed object shape with multiple default values", () => {
+  const form = createFormHelper({
+    defaultValues: [
+      { users: [{ name: "John" }, { name: "Jane" }] }, //
+      { users: [{ name: "John" }, { name: "Hans" }] },
+    ],
+  });
+
+  expect(form.register.users.getIndex(0).name()).toEqual({
+    name: "users[0].name",
+    defaultValue: "John",
+  });
+
+  expect(form.register.users.getIndex(1).name()).toEqual({
+    name: "users[1].name",
+    readOnly: true,
+  });
+});
+
+it("can handle multiple default values with different tree shapes", () => {
+  const form = createFormHelper({
+    defaultValues: [
+      { id: 1, user: { age: 18, name: "Jane" } },
+      { id: 1, user: { age: 18, name2: "Bob" } }, //
+    ],
+  });
+
+  expect(form.register.id()).toEqual({
+    name: "id",
+    defaultValue: "1",
+  });
+
+  expect(form.register.user.name()).toEqual({
+    name: "user.name",
+    readOnly: true,
+  });
+
+  expect(form.register.user.age()).toEqual({
+    name: "user.age",
+    defaultValue: "18",
+  });
+});
+
+it("accepts a callback to get the correct defualt value when using an unordered index", () => {
+  const form = createFormHelper({
+    defaultValues: {
+      users: [
+        { id: 456, name: "Bob" },
+        { id: 123, name: "Jane" }, //
+      ],
+    },
+  });
+
+  expect(form.register.users.getIndex(123, ({ id }) => id).name()).toEqual({
+    name: "users[123].name",
+    defaultValue: "Jane",
+  });
+
+  expect(form.register.users.getIndex(456, ({ id }) => id).name()).toEqual({
+    name: "users[456].name",
+    defaultValue: "Bob",
+  });
+});
+
+it("throws an error if the accessed property is a symbol", () => {
+  const symbol = Symbol("Hey");
+
+  const form = createFormHelper({
+    defaultValues: {
+      [symbol]: "value",
+    },
+  });
+
+  expect(() => form.register[symbol]()).toThrowError("Symbol properties are not supported");
 });
